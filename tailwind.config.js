76ca36cf0d3388779a80400a1f499f01da8601bd
Luis Fernando Manriquez <luismanriquez13@gmail.com>
2025-06-29 13:50:54 -0700

    added styles and initialized app

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure all relevant file types are included
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
